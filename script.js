e3a7433537ecbb9525b3afe53d71fecf4fe1e65e
Julia Saenz <julisaenz99@gmail.com>
2020-11-22 14:43:16 -0300

    04

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ function iniciar() {
   var marcador = contextoAR.crearMarcador('./marcadores/gatito.patt','wabi');
   var maceta = new THREE.Object3D();
   cargarModelo('./modelo/suculenta.glb', maceta);
-  maceta.scale.x = 0.5;
-  maceta.scale.y = 0.5;
-  maceta.scale.z = 0.5;
+  maceta.scale.x = 0.1;
+  maceta.scale.y = 0.1;
+  maceta.scale.z = 0.1;
   marcador.add(maceta);
 
   /* loader = new GLTFLoader();
